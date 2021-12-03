8816c232a1eb4d3a10612287e9469935d4cf4d2d
Michael Stengel <Mike.Stengel@gmail.com>
2021-12-03 12:11:14 -0600

    Simplify day 1

--- day1/main.go
+++ day1/main.go
@@ -39,23 +39,28 @@ func main() {
 }
 
 func part1(depths []int) int {
-	count := 0
-
-	for i := 0; i < len(depths)-1; i++ {
-		if depths[i+1] > depths[i] {
-			count += 1
-		}
-	}
-
-	return count
+	return calculateDepthWithSpan(depths, 1)
 }
 
 func part2(depths []int) int {
-	count := 0
+	return calculateDepthWithSpan(depths, 3)
+}
 
-	for i := 0; i < len(depths)-3; i++ {
-		sum1 := depths[i] + depths[i+1] + depths[i+2]
-		sum2 := depths[i+1] + depths[i+2] + depths[i+3]
+func calculateDepthWithSpan(depths []int, spanCount int) int {
+	count := 0
+	span := makeRange(0, spanCount-1)
+
+	for i := 0; i < len(depths)-spanCount; i++ {
+		sum1 := 0
+		sum2 := 0
+
+		for k := range span {
+			sum1 += depths[i+k]
+		}
+
+		for k := range span {
+			sum2 += depths[i+k+1]
+		}
 
 		if sum2 > sum1 {
 			count += 1
@@ -63,6 +68,14 @@ func part2(depths []int) int {
 	}
 
 	return count
+}
+
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
 }
 
 func convertToDepths(inputs []string) ([]int, error) {
